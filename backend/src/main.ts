79c3927343e9a5a9b4c0ccb84a11346f89dca572
Tempesto_S <bro74rus1@mail.ru>
2026-04-22 21:59:15 +0500

    fast comm

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,26 +1,10 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   await app.listen(process.env.PORT ?? 3000);
-// }
-// bootstrap();
-
-
-
-
-
-
-
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // Добавьте эти строки
+
   app.enableCors({
     // origin: 'http://localhost:3001', // или true для разрешения всех доменов
     origin: true,
